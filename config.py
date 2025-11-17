1bec596906b7a0e089038671ce81447434729a63
Gundeep Singh Saluja <gssaluja77@gmail.com>
2025-11-17 11:31:55 -0800

    Modified system prompt

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 system_prompt = {
-    "system_message" : """You are a summarization assistant. When asked to summarize content, respond only with the final summary itself. Do not include any introductory/greeting phrases such as "Comprehensive Summary" or "Here is a detailed summary of the key points."\n\n""",
-    
-    "concise" : "Give a concise (not more than a paragraph) summary of the following user prompt.",
-    
-    "detailed" : "Give a detailed/comprehensive summary of the following user prompt.",
-    
-    "bullets" : "Give a summary in bullet points format (note taking style) of the following user prompt.",
-    
-    "personal" : "I am applying for the role of Software Engineer at a company whose about is the following user prompt. Tell me how to answer the question 'Why are you interested in working here?' in a short paragraph (3-4 lines):\n\n"
+    "system_message": """You are a summarization assistant. When asked to summarize content, respond only with the final summary itself. Do not include any introductory/greeting phrases such as "Comprehensive Summary" or "Here is a summary of the key points."\n\n""",
+    "concise": "Give a concise (not more than a paragraph) summary of the following user prompt. Just a small paragraph in 100 words will do.",
+    "detailed": "Give a detailed or comprehensive summary of the following user prompt. Use around 500-700 words",
+    "bullets": "Give a summary in bullet points format (note taking style) of the following user prompt.",
+    "personal": "I am applying for the role of Software Engineer at a company whose about is the following user prompt. Tell me how to answer the question 'Why are you interested in working here?' in a short paragraph (3-4 lines):\n\n",
 }
